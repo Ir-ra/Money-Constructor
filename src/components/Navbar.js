9ad2197e1e4c98a237086ca8f14917db91ed24dd
Ir-ra <chapter.ku@ukr.net>
2022-11-08 13:22:55 +0200

    FB stay auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
 import styles from './Navbar.module.css'
 import {Link} from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout'
+import {useAuth} from '../hooks/useAuth'
+import { Fragment } from 'react'
 
 function Navbar() {
     const {logOut} = useLogout()
+    const { user } = useAuth()
 
     return ( 
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>myMoney</li>
 
+            {!user && (
+            <Fragment>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/signUp'>SignUp</Link></li>
+            </Fragment>
+            )}
 
-                <li className='btn' onClick={logOut}>Logout</li>
+            {user && (
+                <Fragment>
+                <li>Hello, {user.displayName}</li>
+                <button className='btn' onClick={logOut}>Logout</button>
+                </Fragment>
+                )}
+                
             </ul>
         </nav>
      );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { projectAuth} from "../firebase/config";
 
 export const AuthContext = createContext()
 
@@ -8,6 +9,8 @@ export const authReducer = (state, action) => {
             return { ...state, user: action.payload }
         case 'LOGOUT':
             return { ...state, user: null}
+        case 'AUTH_IS_READY':
+            return { ...state, user: action.payload, authIsReady: true}
         default:
             return state;
     }
@@ -17,7 +20,17 @@ export const authReducer = (state, action) => {
 export function AuthContextProvider({ children }) {
     const [state, dispatch] = useReducer(authReducer, {
         user: null,
+        authIsReady: false    //це щоб браузер залишав залоганим як і на ФБ
     })
+
+//Коли AuthContextProvider вперше прикріпляється, то викорстовємо нижче
+    useEffect(()=>{
+        const unsub = projectAuth.onAuthStateChanged((user) => {  //ця ф-я комунікую з ФБ, і каже йому хто і коли залоганий
+            dispatch({type:'AUTH_IS_READY', payload: user})
+            unsub()
+        })
+    },[])
+
 console.log('AuthContext state:', state)
 
     return (
