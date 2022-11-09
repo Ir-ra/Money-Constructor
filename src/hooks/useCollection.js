3d8a894c5ea2a6217b9b6f82e363739f8fb7e756
Ir-ra <chapter.ku@ukr.net>
2022-11-09 10:31:19 +0200

    useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -6,6 +6,37 @@ import { useEffect, useState } from "react";
 
 const useCollection = (collectioN) => {
     
+    const [error, setError] = useState(null)
+    const [documents, setDocuments] = useState(null)
+    
+    //як тільки зміниться конкретна коллекція, то тут перезапишуться данні
+    useEffect(() => {
+
+      let ref =  projectFirestore.collection(collectioN)
+
+      const unsubscribe = ref.onSnapshot( snapshot => {
+        
+        let results = []
+        snapshot.docs.forEach(doc => {
+            results.push({...doc.data(), id: doc.id}) //({name, amount, uid}) типу це і є doc
+        })
+
+        //update state
+        setDocuments(results)
+        setError(null)
+      },
+
+      //другий аргумент ф-ї unsubscribe
+      (error) => {
+        setError('Could not fetch the data')
+      })
+
+      //unsubscribe on unmount
+      return () => unsubscribe()
+
+    }, [collectioN])
+
+    return {documents, error}
 }
 
 export default useCollection;
