8ea8f8b6e792892a6fdb15fb9f3cc3471e6e1740
Ir-ra <chapter.ku@ukr.net>
2022-11-09 18:21:54 +0200

    FB rules

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,14 +16,15 @@ function Home() {
     return ( 
         <div className={styles.container}>
            
+           <div className={styles.sidebar}>
+                <TransactionForm uid={user.uid}/>
+            </div>
             <div className={styles.content}>
             {error && <p>{error}</p>}
             {documents && <TransactionList transactions={documents} />}
             </div>
 
-            <div className={styles.sidebar}>
-                <TransactionForm uid={user.uid}/>
-            </div>
+            
 
         </div>
      );
